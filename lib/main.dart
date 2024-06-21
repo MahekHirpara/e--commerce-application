4441fd7912343b1485042ca2cf12e4b023e0cf1f
KRIS04 <mahekhirpara63@gmail.com>
2024-06-21 12:33:32 +0530

    init commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,10 @@ import 'package:e_commerce_app/utils/theme/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'features/authentication/screens/on_boarding_screens/onbording.dart';
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+Future<void> main() async{
+  await Firebase.initializeApp(options : DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
